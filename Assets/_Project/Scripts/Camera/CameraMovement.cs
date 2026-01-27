af8ba1d5ed09d6b428c2242c38da90e6f8e8b101
Aji <thowepic@hotmail.com>
2026-01-27 15:28:43 +0100

    Basic Camera Implementation
    Currently tried two different methods; Having one set speed variable to control how fast the camera pans and one that sticks to where you first pressed and then moved from there. The first one needs separate values for each phone and resolution, which will be problematic. The second one is not functioning correctly at the moment and might be beyond my expertise.

---
+++ Assets/_Project/Scripts/Camera/CameraMovement.cs
@@ -0,0 +1,61 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace FG_GP2_T3
+{
+    public class CameraMovement : MonoBehaviour
+    {
+        [Header("Speed Variables")]
+        [SerializeField] private float moveSpeed = 1f;
+
+        [Header("Input Reference")]
+        [SerializeField] private InputActionReference dragAction;
+        [SerializeField] private InputActionReference deltaAction;
+
+        private float moveSpeedOffset;
+
+        void OnEnable()
+        {
+            dragAction.action.Enable();
+            deltaAction.action.Enable();
+        }
+
+        void OnDisable()
+        {
+            dragAction.action.Disable();
+            deltaAction.action.Disable();
+        }
+
+        void OnValidate()
+        {
+            moveSpeedOffset = moveSpeed * 0.01f;
+        }
+
+        void LateUpdate()
+        {
+            if (dragAction.action.IsPressed())
+            {
+                Vector2 delta = deltaAction.action.ReadValue<Vector2>();
+                if (delta != Vector2.zero)
+                {
+                    MoveCamera(delta);
+                }
+            }
+        }
+
+        void MoveCamera(Vector2 delta)
+        {
+            Vector3 forward = transform.up;
+            Vector3 right = transform.right;
+
+            forward.y = 0;
+            right.y = 0;
+            forward.Normalize();
+            right.Normalize();
+
+            Vector3 move = (right * delta.x + (forward * delta.y * 2f)) * moveSpeedOffset;
+            transform.position -= move;
+        }
+        
+    }
+}
