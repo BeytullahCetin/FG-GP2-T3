5bbc3d99cb65eb731b8a2fb97c248785978345b3
Beytullah Çetin <44231507+BeytullahCetin@users.noreply.github.com>
2026-01-31 00:04:36 +0100

    add spiral animation

--- Assets/_Project/Scripts/TileAnimationTest.cs
+++ Assets/_Project/Scripts/TileAnimationTest.cs
@@ -11,40 +11,50 @@ namespace FG_GP2_T3
     public class TileAnimationTest : MonoBehaviour
     {
         [SerializeField] Transform cellsParent;
-        [SerializeField] float dropDuration = .5f;
+        [SerializeField] float dropHeight = 100f;
+        [SerializeField] float dropDuration = 1f;
         [SerializeField] float delayBetweenCells = .1f;
 
         [SerializeField] List<HexCell> cells = new List<HexCell>();
 
-        [Button]
-        public void AnimateCells()
+        void Awake()
         {
-            DisableAllCells();
-            foreach (Transform child in cellsParent)
-            {
-                child.position = new Vector3(child.position.x, 50, child.position.z);
-            }
-
-            GetHexCellReferances();
-
-            Sequence seq = DOTween.Sequence();
-            int cellIndex = 0;
-            foreach (HexCell cell in cells)
-            {
-                cell.transform.DOMoveY(0, dropDuration)
-                    .SetDelay(delayBetweenCells * cellIndex++)
-                    .OnStart(() => cell.gameObject.SetActive(true));
-            }
+            cells = cellsParent.GetComponentsInChildren<HexCell>(true).ToList();
         }
 
         [Button]
-        public void GetHexCellReferances()
+        public void SetSpiralAnimation()
         {
-            Debug.Log(cellsParent.name);
+            cells = cells
+                .OrderBy(cell => Math.Abs(cell.Coordinates.Q) + Math.Abs(cell.Coordinates.R) + Math.Abs(cell.Coordinates.S))
+                .ThenBy(cell => Math.Atan2(cell.Coordinates.R, cell.Coordinates.Q))
+                .ToList();
+        }
 
-            cells = cellsParent.GetComponentsInChildren<HexCell>(true)
+        [Button]
+        public void SetClosestAnimation()
+        {
+            cells = cells
                 .OrderBy(cell => Math.Abs(cell.Coordinates.Q) + Math.Abs(cell.Coordinates.R) + Math.Abs(cell.Coordinates.S))
                 .ToList();
+        }
+
+        [Button]
+        public void PlayAnimation()
+        {
+            DisableAllCells();
+            Sequence seq = DOTween.Sequence();
+            for (int i = 0; i < cells.Count; i++)
+            {
+                HexCell cell = cells[i];
+
+                cell.transform.position = new Vector3(cell.transform.position.x, dropHeight, cell.transform.position.z);
+                cell.transform
+                    .DOMoveY(0, dropDuration)
+                    .SetDelay(delayBetweenCells * i)
+                    .SetEase(Ease.OutBack)
+                    .OnStart(() => cell.gameObject.SetActive(true));
+            }
         }
 
         public void ResetCellPositions()
