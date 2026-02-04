3741cbd519e54e7ef95eb27688e349b4c776b58a
Delilah Beam <delilah.beam@edu.futuregames.se>
2026-02-04 16:27:56 +0100

    Started linking up global sounds and parameter changes to the fmod stuff through the audio managers

--- Assets/_Project/Scripts/Audio/FmodGlobalParamManager.cs
+++ Assets/_Project/Scripts/Audio/FmodGlobalParamManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using FMODUnity;
 using UnityEngine;
@@ -23,11 +24,79 @@ namespace FG_GP2_T3
             Instance = this;
         }
 
+        private void OnEnable()
+        {
+            EventManager.Register<OnVolumeEvent>(OnVolumeEvent);
+            EventManager.Register<OnPauseMenuEvent>(OnPauseMenuEvent);
+            EventManager.Register<OnCoreDamageEvent>(OnCoreDamageEvent);
+        }
+
+        private void OnDisable()
+        {
+            EventManager.Unregister<OnVolumeEvent>(OnVolumeEvent);
+            EventManager.Unregister<OnPauseMenuEvent>(OnPauseMenuEvent);
+            EventManager.Unregister<OnCoreDamageEvent>(OnCoreDamageEvent);
+        }
+
 
         public void SetParameter(FmodGlobalParamType globalParamType, float value)
         {
             string paramName = globalParams.Find(x => x.globalParamType == globalParamType).paramName;
             RuntimeManager.StudioSystem.setParameterByName(paramName, value);
+        }
+
+        
+        private void OnVolumeEvent(OnVolumeEvent args)
+        {
+            VolumeType volumeType = args.VolumeType;
+            float volume = args.Volume01;
+            
+            switch (volumeType)
+            {
+                case VolumeType.VolumeMain:
+                    SetParameter(FmodGlobalParamType.VolumeMain, volume);
+                    break;
+                case VolumeType.VolumeMusic:
+                    SetParameter(FmodGlobalParamType.VolumeMusic, volume);
+                    break;
+                case VolumeType.VolumeAmbience:
+                    SetParameter(FmodGlobalParamType.VolumeAmbience, volume);
+                    break;
+                case VolumeType.VolumeSfxAuto:
+                    SetParameter(FmodGlobalParamType.VolumeSfxAuto, volume);
+                    break;
+                case VolumeType.VolumeSfxInteraction:
+                    SetParameter(FmodGlobalParamType.VolumeSfxInteraction, volume);
+                    break;
+                case VolumeType.VolumeNarration:
+                    SetParameter(FmodGlobalParamType.VolumeNarration, volume);
+                    break;
+                case VolumeType.VolumeUi:
+                    SetParameter(FmodGlobalParamType.VolumeUi, volume);
+                    break;
+            }
+        }
+
+        private void OnPauseMenuEvent(OnPauseMenuEvent args)
+        {
+            UIEventType uiEventType = args.EventType;
+
+            switch (uiEventType)
+            {
+                case UIEventType.Open:
+                    SetParameter(FmodGlobalParamType.IsPaused, 1);
+                    break;
+                case UIEventType.Close:
+                    SetParameter(FmodGlobalParamType.IsPaused, 0);
+                    break;
+            }
+        }
+        
+        private void OnCoreDamageEvent(OnCoreDamageEvent args)
+        {
+            float health = args.CurrentHealth01;
+            
+            SetParameter(FmodGlobalParamType.PoeHealth, health);
         }
     }
 }
