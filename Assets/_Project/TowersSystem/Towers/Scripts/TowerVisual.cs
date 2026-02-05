db509feb9b7f4c28c6fbe64d5b45521aca822b4b
GunaSekhar <55795755+gunashekar457@users.noreply.github.com>
2026-02-05 15:01:26 +0530

    updated  TowerVisuals Script

--- Assets/_Project/TowersSystem/Towers/Scripts/TowerVisual.cs
+++ Assets/_Project/TowersSystem/Towers/Scripts/TowerVisual.cs
@@ -1,11 +1,14 @@
 using UnityEngine;
+using NaughtyAttributes;
 
 namespace FG_GP2_T3
 {
     public class TowerVisual : MonoBehaviour
     {
+        [Expandable]
         [SerializeField] TowerData towerData;
 
+        
         public TowerData TowerData => towerData;
     }
 }
